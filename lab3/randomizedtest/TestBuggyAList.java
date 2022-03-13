95f803205d34f0726dc5740e04537a5b4c942019
f2xiao <fxiao.mecheng@gmail.com>
2022-03-13 19:53:43 -0400

    Add testThreeAddThreeRemove

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,19 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+        BuggyAList<Integer> bList = new BuggyAList<>();
+        AListNoResizing<Integer> aList = new AListNoResizing<>();
+        int[] three = {4,5,6};
+        for (int i = 0; i < three.length; i++) {
+            bList.addLast(three[i]);
+            aList.addLast(three[i]);
+        }
+
+        assertEquals(bList.size(), aList.size());
+        assertEquals(bList.removeLast(), aList.removeLast());
+        assertEquals(bList.removeLast(), aList.removeLast());
+        assertEquals(bList.removeLast(), aList.removeLast());
+    }
 }
