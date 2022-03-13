85877aafcffbed37c5bdf7833bda54198645303e
f2xiao <fxiao.mecheng@gmail.com>
2022-03-13 15:57:30 -0400

    Add createNs,calcTime,createTimes,copyNs and timeAListConstruction

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -21,7 +22,48 @@ public class TimeAList {
         timeAListConstruction();
     }
 
+    public static AList<Integer> createNs(int n0, int numOfTests){
+        AList<Integer> Ns = new AList<>();
+        for (int i = 0; i < numOfTests; i++) {
+            Ns.addLast((int) (n0 * Math.pow(2, i)));
+        }
+
+        return Ns;
+    }
+
+    public static Double calcTime(int size){
+        AList<Integer> list = new AList<>();
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < size; i++) {
+            list.addLast(i);
+        }
+        return sw.elapsedTime();
+    }
+
+    public static AList<Double> createTimes(AList<Integer> Ns){
+        AList<Double> times = new AList<>();
+        for (int i = 0; i <Ns.size() ; i++) {
+            times.addLast(calcTime(Ns.get(i)));
+        }
+        return times;
+    }
+    public static AList<Integer> copyNs(AList<Integer> Ns){
+        AList<Integer> opCounts = new AList<>();
+        for (int i = 0; i <Ns.size() ; i++) {
+            opCounts.addLast(Ns.get(i));
+        }
+
+        return opCounts;
+    }
+
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        System.out.println("start");
+        int n0 = 1000;
+        int numOfTests = 8;
+        AList<Integer> Ns = createNs(n0,numOfTests);
+        AList<Double> times = createTimes(Ns);
+        AList<Integer> opCounts = copyNs(Ns);
+        printTimingTable(Ns,times,opCounts);
     }
 }
