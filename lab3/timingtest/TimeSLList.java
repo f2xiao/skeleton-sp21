0823ceb5d9c2dc68f336d7d41cfa2f00610de03d
f2xiao <fxiao.mecheng@gmail.com>
2022-03-13 19:26:57 -0400

    Add createNs,calcTime,createTimes,createCounts and timeGetLast

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -21,8 +21,41 @@ public class TimeSLList {
         timeGetLast();
     }
 
+    public static AList<Double> createTimes(AList<Integer> Ns, int M){
+        AList<Double> times = new AList<>();
+        for (int i = 0; i <Ns.size() ; i++) {
+            times.addLast(calcTime(Ns.get(i), M));
+        }
+        return times;
+    }
+    public static Double calcTime(int size,int M){
+        SLList<Integer> list = new SLList<>();
+        for (int i = 0; i < size; i++) {
+            list.addLast(i);
+        }
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < M; i++) {
+            list.getLast();
+        }
+        return sw.elapsedTime();
+    }
+    public static AList<Integer> createCounts(int numOfTests, int M){
+        AList<Integer> opCounts = new AList<>();
+        for (int i = 0; i < numOfTests; i++) {
+            opCounts.addLast(M);
+        }
+        return opCounts;
+    }
+
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        int n0 = 1000;
+        int numOfTests = 8;
+        int M = 10000;
+        AList<Integer> Ns = TimeAList.createNs(n0,numOfTests);
+        AList<Double> times = createTimes(Ns, M);
+        AList<Integer> opCounts = createCounts(numOfTests,M);
+        printTimingTable(Ns,times,opCounts);
     }
 
 }
