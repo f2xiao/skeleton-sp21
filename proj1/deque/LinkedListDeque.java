c968e1895895332a5184bef16bd9d89052f5f276
f2xiao <fxiao.mecheng@gmail.com>
2022-03-24 14:02:12 -0400

    Add LinkedListDeque class and its constructors

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,45 @@
+package deque;
+
+public class LinkedListDeque<T> {
+
+    private class StuffNode{
+        public StuffNode prev;
+        public T item;
+        public StuffNode next;
+
+        public StuffNode(T x, StuffNode n){
+            item = x;
+            next = n;
+        }
+    }
+
+    private StuffNode sentinel;
+    private int size;
+
+    /** Create an empty deque*/
+    public LinkedListDeque(){
+        sentinel = new StuffNode(null,null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size =0;
+    }
+
+    /** Creates a deque with item as the first item*/
+    public LinkedListDeque(T item){
+        sentinel=new StuffNode(null,null);
+        sentinel.next = new StuffNode(item, sentinel);
+        sentinel.next.prev = sentinel;
+        sentinel.prev = sentinel.next;
+        size =1;
+    }
+
+    /** Add x as the first item in the deque*/
+    public void addFirst(T item){
+
+    }
+
+
+
+
+
+}
